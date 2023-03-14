5bbffd4ba6959d645e477f73cdb3897393985f36
PJ <87795990+pauldidonatojr@users.noreply.github.com>
2023-03-14 13:28:45 -0400

    push me

--- src/components/NewsApi.js
+++ src/components/NewsApi.js
@@ -3,6 +3,9 @@ import axios from 'axios'
 import styled from 'styled-components'
 
 const ArticleCard = styled.div`
+ display: grid;
+ justify-content: center;
+ align-items: center;
  margin-bottom: 20px;
  border: 1px solid #ccc;
  padding: 20px;
@@ -48,6 +51,31 @@ const OverlayContent = styled.div`
  overflow-y: scroll;
 `
 
+const OverlayCloseButton = styled.button`
+ position: absolute;
+ top: 10px;
+ right: 10px;
+ padding: 10px;
+ background-color: #ccc;
+ border: none;
+ border-radius: 5px;
+ &:hover {
+  cursor: pointer;
+ }
+`
+const Button = styled.button`
+ background-color: #0077cc;
+ color: white;
+ border: none;
+ padding: 10px 20px;
+ border-radius: 5px;
+ font-size: 16px;
+ width: 40vh;
+ cursor: pointer;
+ &:hover {
+  background-color: #005fa3;
+ }
+`
 function NewsList() {
  const [articles, setArticles] = useState([])
  const [selectedArticle, setSelectedArticle] = useState(null)
@@ -70,7 +98,7 @@ function NewsList() {
   setSelectedArticle(article)
  }
 
- function handleOverlayClick() {
+ function handleCloseOverlay() {
   setSelectedArticle(null)
  }
 
@@ -78,19 +106,21 @@ function NewsList() {
   <div>
    <h2>Latest News Articles</h2>
    {articles.map((article) => (
-    <ArticleCard
-     key={article.publishedAt}
-     onClick={() => handleArticleClick(article)}
-    >
+    <ArticleCard key={article.publishedAt}>
      <ArticleTitle>{article.title}</ArticleTitle>
      <ArticleContent>{article.content}</ArticleContent>
+
+     <Button onClick={() => handleArticleClick(article)}> Open</Button>
     </ArticleCard>
    ))}
    {selectedArticle && (
-    <Overlay onClick={handleOverlayClick}>
+    <Overlay>
      <OverlayContent>
       <ArticleTitle>{selectedArticle.title}</ArticleTitle>
       <p>{selectedArticle.content}</p>
+      <OverlayCloseButton onClick={handleCloseOverlay}>
+       Close
+      </OverlayCloseButton>
      </OverlayContent>
     </Overlay>
    )}
